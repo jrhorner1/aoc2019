95989673e5ba9cba6cfc051c94eaf23626200fb5
Jesse Horner <jrhorner1@liberty.edu>
2019-12-19 14:56:46 -0500

    Removed debugging statements

--- utils/computer.go
+++ utils/computer.go
@@ -1,7 +1,5 @@
 package utils
 
-import "fmt"
-
 type Computer struct {
     memory          *[]int
     pointer         int
@@ -12,16 +10,12 @@ type Computer struct {
 }
 
 const (
-    DEBUG = false
     memoryMultiplier = 50
 )
 
 func NewComputer(program *[]int) Computer {
     memory := make([]int, len(*program) * memoryMultiplier)
     copy(memory, *program)
-    if DEBUG {
-        fmt.Printf("DEBUG: %s:%d\n","NewComputer() memory",memory)
-    }
 
     return Computer{
         memory: &memory, 
@@ -37,9 +31,6 @@ loop:
     for {
         c.instruction = c.Read(1)
         opcode := c.instruction % 100
-        if DEBUG {
-            fmt.Printf("DEBUG: %s:%d\n","Run() opcode",opcode)
-        }
         switch opcode {
         case 1: c.Add()
         case 2: c.Multiply()
@@ -68,30 +59,17 @@ func (c *Computer) Parameter(mode int) int {
         address = (*c.memory)[c.pointer] + c.relativeBase
     }
     c.pointer++
-    if DEBUG {
-        fmt.Printf("DEBUG: %s:%d\n","Parameter() address",address)
-        fmt.Printf("DEBUG: %s:%d\n","Parameter() c.pointer",c.pointer)
-    }
     return address
 }
 
 func (c *Computer) Read(mode int) int {
     address := c.Parameter(mode) 
-    if DEBUG {
-        fmt.Printf("DEBUG: %s:%d\n","Read() address",address)
-        fmt.Printf("DEBUG: %s:%d\n","Read() memory",(*c.memory)[address])
-    }
     return (*c.memory)[address]
 }
 
 func (c *Computer) Write(value int, mode int) {
     address := c.Parameter(mode)
     (*c.memory)[address] = value
-    if DEBUG {
-        fmt.Printf("DEBUG: %s:%d\n","Write() address",address)
-        fmt.Printf("DEBUG: %s:%d\n","Write() value",value)
-        fmt.Printf("DEBUG: %s:%d\n","Write() memory",(*c.memory)[address])
-    }
     return
 }
 
@@ -120,9 +98,6 @@ func (c *Computer) GetInput() chan int {
 
 func (c *Computer) PutOutput() {
     out := c.Read((c.instruction / 100) % 10)
-    if DEBUG {
-        fmt.Printf("DEBUG: %s:%d\n","PutOutput() out",out)
-    }
     c.output <- out
 }
 
@@ -136,11 +111,6 @@ func (c *Computer) JumpIfTrue() {
     if test != 0 {
         c.pointer = newPointer
     }
-    if DEBUG {
-        fmt.Printf("DEBUG: %s:%d\n","JumpIfTrue() test",test)
-        fmt.Printf("DEBUG: %s:%d\n","JumpIfTrue() newPointer",newPointer)
-        fmt.Printf("DEBUG: %s:%d\n","JumpIfTrue() c.pointer",c.pointer)
-    }
 }
 
 func (c *Computer) JumpIfFalse() {
@@ -148,11 +118,6 @@ func (c *Computer) JumpIfFalse() {
     newPointer := c.Read((c.instruction / 1000) % 10)
     if test == 0 {
         c.pointer = newPointer
-    }
-    if DEBUG {
-        fmt.Printf("DEBUG: %s:%d\n","JumpIfFalse() test",test)
-        fmt.Printf("DEBUG: %s:%d\n","JumpIfFalse() newPointer",newPointer)
-        fmt.Printf("DEBUG: %s:%d\n","JumpIfFalse() c.pointer",c.pointer)
     }
 }
 
@@ -179,10 +144,6 @@ func (c *Computer) Equals() {
 func (c *Computer) RelativeBaseOffset() {
     offset := c.Read((c.instruction / 100) % 10)
     c.relativeBase += offset
-    if DEBUG {
-        fmt.Printf("DEBUG: %s:%d\n","RelativeBaseOffset() offset",offset)
-        fmt.Printf("DEBUG: %s:%d\n","RelativeBaseOffset() c.relativeBase",c.relativeBase)
-    }
 }
 
 
